e1cf1f7e6bdcbaa7c3c530077ec5b3ac516cb8bc
Kiri111enz <omelyanenkokirill@gmail.com>
2021-08-23 21:05:35 +0700

    Added shooting animation with recoil.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -2,20 +2,33 @@ using UnityEngine;
 
 namespace ShootEm
 {
+    [RequireComponent(typeof(Animator))]
     public class Gun : MonoBehaviour
     {
         [SerializeField] private Vector3 _shootPositionOffset;
 
         [SerializeField] private Bullet _bulletPrefab;
 
+        [SerializeField] private string _fireTriggerName = "Fired";
+        private Animator _animator;
+
         private Vector3 ShootPosition => transform.position + _shootPositionOffset;
 
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+        
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = Color.red;
             Gizmos.DrawRay(ShootPosition, transform.forward * 100);
         }
         
-        public void Shoot() => Instantiate(_bulletPrefab, ShootPosition, transform.rotation);
+        public void Shoot()
+        {
+            Instantiate(_bulletPrefab, ShootPosition, transform.rotation);
+            _animator.SetTrigger(_fireTriggerName);
+        }
     }
 }
